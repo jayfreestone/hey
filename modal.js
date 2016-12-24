f0f0e04be2ff414d53a57db022d99a59193d1414
Jay Freestone <mail@jayfreestone.com>
2016-12-24 17:29:18 +0000

    Init.

---
+++ modal.js
@@ -0,0 +1,144 @@
+(() => {
+  const heyModal = {
+    body: null,
+    elem: null,
+    target: null,
+    content: {
+      title: null,
+      body: null,
+    },
+    visibleClass: 'modal--is-visible',
+    bodyOverflowClass: 'modal-body-no-scroll',
+    init () {
+      this.body = document.querySelector('body');
+      this.setTarget();
+      this.build();
+      this.removeTarget();
+      this.bindEvents();
+    },
+    build () {
+      const c = {};
+
+      // Wrapper
+      c.wrapper = document.createElement('div');
+      c.wrapper.classList.add('modal');
+
+      // Dialog
+      c.dialog = document.createElement('div');
+      c.dialog.classList.add('modal__dialog');
+
+      // Header
+      c.header = document.createElement('div');
+      c.header.classList.add('modal__header');
+
+      // Title
+      c.title = document.createElement('h3');
+
+      // Body
+      c.body = document.createElement('div');
+      c.body.classList.add('modal__body');
+
+      // Close button
+      c.closeBtn = document.createElement('button');
+      c.closeBtn.classList.add('modal__close');
+
+      // Build modal
+      c.header.appendChild(c.title);
+      c.header.appendChild(c.closeBtn);
+      c.dialog.appendChild(c.header);
+      c.dialog.appendChild(c.body);
+      c.wrapper.appendChild(c.dialog);
+
+      this.comp = c;
+
+      // Update content
+      this.populate();
+
+      // Add to DOM
+      this.body.appendChild(c.wrapper);
+    },
+    populate() {
+      const self = this;
+
+      // TODO could probably be re-written to accommodate mix of JS/non-js content, or just made simpler
+      for (const el in this.content) {
+        if (!this.content[el]) {
+          const domElem = self.target.querySelector(`[data-modal-${el}]`);
+
+          if (domElem) {
+            self.content[el] = domElem.innerHTML;
+          }
+        }
+
+        this.comp[el].innerHTML = this.content[el];
+      }
+    },
+    setTarget() {
+      const dataTarget = this.elem.getAttribute('data-modal');
+
+      // If a data attribute is set with a target
+      if (dataTarget) {
+        this.target = document.querySelector(dataTarget);
+      }
+    },
+    // Remove the original target
+    removeTarget() {
+      this.target.remove();
+    },
+    bindEvents() {
+      this.elem.addEventListener('click', e => {
+        e.preventDefault();
+        this.open();
+      });
+
+      this.comp.wrapper.addEventListener('click', this.close.bind(this));
+      this.comp.closeBtn.addEventListener('click', this.close.bind(this));
+
+      this.comp.dialog.addEventListener('click', (e) => {
+        e.stopPropagation();
+      });
+    },
+    open() {
+      this.comp.wrapper.classList.add(this.visibleClass);
+      this.setPageScroll(false);
+    },
+    close() {
+      this.comp.wrapper.classList.remove(this.visibleClass);
+
+      const closeOver = () => {
+        this.setPageScroll(true);
+        this.comp.wrapper.removeEventListener('transitionend', closeOver);
+      };
+
+      this.comp.wrapper.addEventListener('transitionend', closeOver);
+    },
+    setPageScroll(scrollable = false) {
+      if (scrollable) {
+        this.body.classList.remove(this.bodyOverflowClass);
+      } else {
+        this.body.classList.add(this.bodyOverflowClass);
+      }
+    },
+  };
+
+  window.heyModal = (elem, options) => {
+    // Create a new modal object
+    const newModal = Object.assign(Object.create(heyModal), {
+      elem,
+    }, options);
+
+    // Run initialisation
+    newModal.init();
+
+    // Return our new modal
+    return newModal;
+  }
+})();
+
+const myModal = heyModal(document.querySelector('.modal-trigger'));
+
+const lesserModal = heyModal(document.querySelector('.less-great-modal-trigger'), {
+  content: {
+    title: 'Lesser title override',
+  },
+});
